2d696b2090cd58cf0a3f4e8a0546cee284051bfd
Rahel_Hagos <hagos.rahelh@gmail.com>
2020-09-07 00:40:27 -0400

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,24 @@
+module Dance
+
+  # attr_accessor :name
+
+  # def initialize 
+  #   @name=name
+  # end 
+
+  def twirl
+   return  "I'm twirling!"
+  end 
+
+  def jump
+    return "Look how high I'm jumping!"
+  end 
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end 
+
+  def take_a_bow 
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end 
+end 
